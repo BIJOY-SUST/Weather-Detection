d95e5271f8a0587786fac13c1408a25c029228e5
BIDDUT SARKER BIJOY <bsbijoy2050@gmail.com>
2019-08-19 19:01:56 +0600

    text fixed

--- public/js/app.js
+++ public/js/app.js
@@ -15,13 +15,16 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'Loading...'
-messageTwo.textContent = ' '
+
 
 weatherForm.addEventListener('submit',( e )=> {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ' '
+
     fetch('/weather?address=' + location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
